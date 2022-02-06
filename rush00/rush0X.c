def5785cf6e19bb54b83e8df9a955b1a073c643c
kferreir <kferreir@student.42sp.org.br>
2022-02-06 16:24:25 +0100

    rush00

---
+++ rush00/rush0X.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush0X.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kferreir <kferreir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 13:11:18 by faaugust          #+#    #+#             */
+/*   Updated: 2022/02/06 15:50:34 by kferreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	func_while(int x, int y);
+
+void	rush01(int x, int y)
+{
+	if (x <= 0 || y <= 0)
+		return ;
+	x--;
+	y--;
+	func_while(x, y);
+}
+
+void	func_while(int x, int y)
+{
+	int	c;
+	int	l;
+
+	l = y;
+	c = 0;
+	while (c <= y)
+	{
+		l = 0;
+		while (l <= x)
+		{
+			if ((c == 0 && l == 0) || (c == y && l == x && c != 0 && l != 0))
+				ft_putchar('/');
+			else if ((c == 0 && l == x) || (c == y && l == 0))
+				ft_putchar('\\');
+			else if (c == 0 || l == 0 || l == x || c == y)
+				ft_putchar('*');
+			else
+				ft_putchar (' ');
+			l++;
+		}
+		c++;
+		ft_putchar ('\n');
+	}	
+}
---
+++ rush00/tmain.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tmain.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kferreir <kferreir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 22:29:29 by kferreir          #+#    #+#             */
+/*   Updated: 2022/02/06 16:19:44 by kferreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	rush01(int x, int y);
+void	ft_putchar(char c);
+
+int	main(void)
+{
+	rush01(5, 3);
+	// ft_putchar('\n');
+	// rush01(5, 1);
+	// ft_putchar('\n');
+	// rush01(1, 1);
+	// ft_putchar('\n');
+	// rush01(1, 5);
+	// ft_putchar('\n');
+	// rush01(4, 4);
+	// ft_putchar('\n');
+	// rush01(5, 5);
+	// ft_putchar('\n');
+	// rush01(123, 42);
+	// ft_putchar('\n');
+	return (0);
+}
